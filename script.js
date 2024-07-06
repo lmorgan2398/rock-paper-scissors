912a570b3adad15bcbaaabca8ae2d45ea961a4f2
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-06 15:40:18 -0500

    Finish script necessary to play through full game
    Completed the game, now fully functional from start to
    end with all buttons necessary. Working on reset button to
    reset the game if a game is in progress or replay the game if
    a full match has already been completed.

--- script.js
+++ script.js
@@ -6,11 +6,20 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let roundNumber = 1;
+let roundNumberText = document.querySelector(".round-number");
+
 let playerScoreText = document.querySelector('.player-score');
 let computerScoreText = document.querySelector('.computer-score');
 
 playerScoreText.textContent = `Your Score: ${playerScore}`;
 computerScoreText.textContent = `Nitwit's Score ${computerScore}`;
+
+let playerChoiceImage = document.querySelector('.player-choice-img');
+let computerChoiceImage = document.querySelector('.computer-choice-img');
+computerChoiceImage.style.display = 'none';
+playerChoiceImage.style.display = 'none';
+
 
 let playerChoice = document.querySelector('.player-choice');
 let playerSelection = '';
@@ -22,19 +31,30 @@ let shearsButton = document.querySelector('.shears-button');
 rockButton.addEventListener('click', () => {
     playerChoice.textContent = 'Rock';
     playerSelection = 'rock';
+    playerChoiceImage.src = "stone.webp";
+    playerChoiceImage.style.display = 'inline';
 });
 
 paperButton.addEventListener('click', () => {
     playerChoice.textContent = 'Paper';
     playerSelection = 'paper';
+    playerChoiceImage.src = "paper.webp";
+    playerChoiceImage.style.display = 'inline';
 });
 
 shearsButton.addEventListener('click', () => {
     playerChoice.textContent = 'Shears';
     playerSelection = 'shears';
+    playerChoiceImage.src = "shears.webp";
+    playerChoiceImage.style.display = 'inline';
 });
 
+let roundInformation = document.querySelector('.round-information');
+roundInformation.textContent = `Hit the 'play round' button once you have made your selection!`;
+
 let playRoundButton = document.querySelector('.play-round-button');
+
+let computerChoice = document.querySelector('.computer-choice');
 
 playRoundButton.addEventListener('click', () => {
     let computerSelection;
@@ -51,17 +71,15 @@ playRoundButton.addEventListener('click', () => {
     }
     getComputerSelection();
 
-    let computerChoice = document.querySelector('.computer-choice');
-
     function playRound (playerSelected, computerSelected) {
-        let roundInformation = document.querySelector('.round-information');
         if (playerSelected === 'rock' || playerSelected ==='paper' || playerSelected === 'shears') {
             let computerChoiceText = computerSelected.slice();
-            console.log(computerChoiceText);
             let = computerChoiceTextUppercase = (computerChoiceText.charAt(0).toUpperCase()) + computerChoiceText.slice(1);
-            console.log(computerChoiceTextUppercase);
             computerChoice.textContent = `${computerChoiceTextUppercase}`;
+
             if (computerSelected === 'rock') {
+                computerChoiceImage.src = "stone.webp";
+                computerChoiceImage.style.display = 'inline';
                 if (playerSelected === 'rock') {
                     roundInformation.textContent = 'Nitwit selected \'rock\'. Tie! No winner for this round.';
                 } else if (playerSelected === 'paper') {
@@ -73,6 +91,8 @@ playRoundButton.addEventListener('click', () => {
                 }
             }
             else if (computerSelected === 'paper') {
+                computerChoiceImage.src = "paper.webp";
+                computerChoiceImage.style.display = 'inline';
                 if (playerSelected === 'paper') {
                     roundInformation.textContent = 'Nitwit selected \'paper\'. Tie! No winner for this round.';
                 } else if (playerSelected === 'shears') {
@@ -84,6 +104,8 @@ playRoundButton.addEventListener('click', () => {
                 }
             }    
             else {
+                computerChoiceImage.src = "shears.webp";
+                computerChoiceImage.style.display = 'inline';
                 if (playerSelected === 'shears') {
                     roundInformation.textContent = 'Nitwit selected \'shears\'. Tie! No winner for this round.';
                 } else if (playerSelected === 'rock') {
@@ -94,185 +116,81 @@ playRoundButton.addEventListener('click', () => {
                     ++computerScore;
                 }
             }
-        }
         playerScoreText.textContent = `Your Score: ${playerScore}`;
         computerScoreText.textContent = `Nitwit's Score ${computerScore}`;
-        setTimeout(function() {
-            playerSelection = '';
-            computerSelection = '';
-            playerChoice.textContent = 'Awaiting your choice...'
-            computerChoice.textContent = `Awaiting Nitwit's choice...`;
-        }, 5000);
+
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        shearsButton.disabled = true;
+        playRoundButton.disabled = true;
+
+        if (computerScore === 5) {
+            roundInformation.textContent = `Game Over! Nitwit wins. Better luck next time!`;
+            roundInformation.style.color = 'greenyellow';
+        } else if (playerScore === 5) {
+            roundInformation.textContent = `Game Over! You win. Congratulations!`
+            roundInformation.style.color = 'greenyellow';
+        } else {
+            let timeleft = 6;
+            let roundTimer = setInterval(function(){
+                --timeleft;
+                roundNumberText.textContent = `Next round in ${timeleft}...`;
+                if(timeleft === 0) {
+                    clearInterval(roundTimer);
+                    playerSelection = '';
+                    computerSelection = '';
+
+                    playerChoice.textContent = 'Awaiting your choice...'
+                    computerChoice.textContent = `Awaiting Nitwit's choice...`;
+
+                    computerChoiceImage.src = '';
+                    computerChoiceImage.style.display = 'none';
+
+                    playerChoiceImage.src = '';
+                    playerChoiceImage.style.display = 'none';
+
+                    ++roundNumber;
+                    roundNumberText.textContent = `Round ${roundNumber}`;
+
+                    rockButton.disabled = false;
+                    paperButton.disabled = false;
+                    shearsButton.disabled = false;
+                    playRoundButton.disabled = false;
+
+                    roundInformation.textContent = `Hit the 'play round' button once you have made your selection!`;
+                }
+            },1000);
+        }
     }
+    }   
     playRound(playerSelection, computerSelection);
-    if (computerScore === 5) {
-        roundInformation.textContent = `Game Over! Nitwit wins. Better luck next time!`
-    } else if (playerScore === 5) {
-        roundInformation.textContent = `Game Over! You win. Congratulations!`
-    };
 });
 
+let resetButton = document.querySelector('.reset-button');
 
+resetButton.addEventListener('click', () => {
+    console.log('working');
+    playerSelection = '';
+    computerSelection = '';
 
+    playerChoice.textContent = 'Awaiting your choice...'
+    computerChoice.textContent = `Awaiting Nitwit's choice...`;
 
+    computerChoiceImage.src = '';
+    computerChoiceImage.style.display = 'none';
 
+    playerChoiceImage.src = '';
+    playerChoiceImage.style.display = 'none';
 
+    roundNumber = 1;
+    console.log(roundNumber);
+    roundNumberText.textContent = `Round 1`;
 
+    playerScore = 0;
+    computerScore = 0;
 
+    playerScoreText.textContent = `Your Score: ${playerScore}`;
+    computerScoreText.textContent = `Nitwit's Score ${computerScore}`;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function playGame() {
-    alert(`Welcome! You are going to play 5 rounds of 'rock, paper, scissors' against the computer. Each round's winner and the running score will be displayed after each round. Good luck!`)
-
-
-
-    function getComputerChoice() {
-        let computerChoice;
-        let number = (Math.random() * 3);
-        if (+number < 1) {
-            computerChoice = 'rock'
-        } else if (+number < 2) {
-            computerChoice = 'paper'
-        } else {
-            computerChoice = 'scissors'
-        };
-        return computerChoice;
-    }
-
-    function getHumanChoice() {
-        let humanChoice;
-        let humanInput = (prompt('Rock, paper, scissors, SHOOT! Please enter your selection for a game of rock, paper, scissors:')).toLowerCase();
-        if (humanInput === 'rock') {
-            humanChoice = 'rock'
-        } else if (humanInput === 'paper') {
-            humanChoice = 'paper'
-        } else if (humanInput === 'scissors') {
-            humanChoice = 'scissors'
-        } else if (humanInput === ' ') {
-            alert('No selection made. Please enter either \'rock\,\' \'paper\,\' or \'scissors.\'');
-        } else {
-            alert('Invalid selection made. Please enter either \'rock\,\' \'paper\,\' or \'scissors.\'')
-        }
-        return humanChoice;
-    }
-
-    function playRound (humanChoice, computerChoice) {
-        let message;
-        if (computerChoice === 'rock') {
-            if (humanChoice === 'rock') {
-                message = 'Computer selected \'rock\'. Tie! No winner for this round.';
-            } else if (humanChoice === 'paper') {
-                message = 'Computer selected \'rock\'. Paper beats rock; you win this round!';
-                ++humanScore;
-            } else {
-                message = 'Computer selected \'rock\'. Rock beats scissors; you lose this round!';
-                ++computerScore;
-            }
-        }
-        else if (computerChoice === 'paper') {
-            if (humanChoice === 'paper') {
-                message = 'Computer selected \'paper\'. Tie! No winner for this round.';
-            } else if (humanChoice === 'scissors') {
-                message = 'Computer selected \'paper\'. Scissors beats paper; you win this round!';
-                ++humanScore;
-            } else {
-                message = 'Computer selected \'paper\'. Paper beats rock; you lose this round!';
-                ++computerScore;
-            }
-        }    
-        else {
-            if (humanChoice === 'scissors') {
-                message = 'Computer selected \'scissors\'. Tie! No winner for this round.';
-            } else if (humanChoice === 'rock') {
-                message = 'Computer selected \'scissors\'. Rock beats scissors; you win this round!';
-                ++humanScore;
-            } else {
-                message = 'Computer selected \'scissors\'. Scissors beats paper; you lose this round!';
-                ++computerScore;
-            }
-        }
-        alert(`${message} \nYour Score: ${humanScore} Computer Score: ${computerScore}`)
-    }
-
-    let computerSelection = getComputerChoice();
-    let humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
-
-    function result() {
-        let winner;
-        if (computerScore === humanScore) {
-            winner = `Tie game! No winners or losers this time.`;
-        } else if (computerScore < humanScore) {
-            winner = `You win! Congratulations!`;
-        } else {
-            winner = `You lose! Better luck next time.`;
-        }
-        return winner;
-    }
-
-    alert(`GAME OVER \n${result()} \nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-
-};
+});
